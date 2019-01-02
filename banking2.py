f0022465a89202934e2e5d116ba2bbf14e3f9523
Koti Vellanki <vellankikoti@gmail.com>
2019-01-02 18:49:02 +0530

    Add files via upload

---
+++ banking2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 21 05:04:43 2018
+
+@author: My Life
+"""
+
+import sys
+
+class Banking:
+    bank = 'SBI'
+    def __init__(self,name,balance=0.0):
+        self.name = name
+        self.balance = balance
+    def deposit(self,amt):
+        self.balance = self.balance + amt
+        print()
+        print('Your Balance after deposit is :',self.balance)
+    def withdraw(self,amt):
+        try:
+            if amt > self.balance:
+                print()
+                print('Insufficient Balance...,your balance is :',self.balance)
+                sys.exit()
+            self.balance = self.balance - amt
+            print()
+            print('Balance after withdraw is:',self.balance)
+        except Exception:
+            self.balance = self.balance - amt
+            print()
+            print('Balance after withdraw is:',self.balance)
+    
+name = input('Enter Your Name :')
+print('{} Bank Welcomes you {}'.format(Banking.bank,name))
+
+customer = Banking(name)
+
+while True:
+    print('Banking Features:\nD-Deposit\nW-Withdraw\nE-Exit')
+    option = input('Enter Your Option:')
+    if option in ['d','D']:
+        amt = float(input('Enter Amount for Deposit:'))
+        customer.deposit(amt)
+    elif option in ['w','W']:
+        amt = float(input('Enter Amount to Withdraw:'))
+        customer.withdraw(amt)
+    elif option in ['e','E']:
+        print()
+        print('Thanks for Banking with {}'.format(Banking.bank))
+        sys.exit()
+    else:
+        print()
+        print('Please Choose the valid option............')
